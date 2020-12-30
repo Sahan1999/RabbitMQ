6ef290c6a5ca5269b3ffa2b8afad412a318099e0
Sahan Thilina <53267271+Sahan1999@users.noreply.github.com>
2020-12-30 19:09:57 +0530

    RabbitMQReceive Service Committed

---
+++ RabbitMQReceiveService/src/test/java/com/adl/RabbitMQReceiveService/RabbitMqReceiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adl.RabbitMQReceiveService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqReceiveServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
